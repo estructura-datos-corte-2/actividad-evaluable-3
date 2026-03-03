f96823306286737909466054b2e2ce961dc65275
juan4231 <jcpuentesl@Libertadores.edu.co>
2026-03-03 04:49:57 +0000

    aqui es la creacion de la clase nodo donde creamos el nodo con sus respectivas lineas y donde tambien creamos el push donde esta simbolo dectectado y  su posicion exacta

---
+++ nodo.java
@@ -0,0 +1,15 @@
+public class nodo{
+    String simbolo;
+    int linea;
+    int columna;
+    nodo siguiente;
+
+    public nodo(String simbolo, int linea, int columna){
+        this.simbolo = simbolo;
+        this.linea = linea;
+        this.columna = columna;
+        this.siguiente = null;
+
+    }
+    
+}
